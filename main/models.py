0fc1a93c858c09ee49d166520969828970b78da5
shreayan98c <shreayan98c@gmail.com>
2020-01-06 12:48:20 +0530

    Making backend changes

--- main/models.py
+++ main/models.py
@@ -3,12 +3,12 @@ from django.db import models
 # Create your models here.
 
 class Article(models.Model):
-	source = models.CharField(max_length=200, null=True, blank=True)
-	author = models.CharField(max_length=200, default="Anonymous", null=True, blank=True)
-	title = models.CharField(max_length=200, null=True, blank=True)
-	description = models.CharField(max_length=500, null=True, blank=True)
-	url = models.CharField(max_length=200, null=True, blank=True)
-	urlToImg = models.CharField(max_length=200, null=True, blank=True)
+	source = models.CharField(max_length=800, null=True, blank=True)
+	author = models.CharField(max_length=800, default="Anonymous", null=True, blank=True)
+	title = models.CharField(max_length=800, null=True, blank=True)
+	description = models.CharField(max_length=800, null=True, blank=True)
+	url = models.CharField(max_length=800, null=True, blank=True)
+	urlToImg = models.CharField(max_length=800, null=True, blank=True)
 	publishedAt = models.DateTimeField('Date Published', null=True, blank=True)
 	content = models.TextField(null=True, blank=True)
 
